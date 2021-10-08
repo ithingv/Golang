ac5f345d290d7f22c60b350901f9322560558e9a
Sanghoon Lee <ithing43@gmail.com>
2021-10-08 21:19:28 +0900

    Update ex_1.go

--- channels/ex_1.go
+++ channels/ex_1.go
@@ -22,7 +22,7 @@ func main() {
 func checkLink(link string) {
 	_, err := http.Get(link)
 	if err != nil {
-		fmt.Println(link, "might be down!")
+		fmt.Println(link, " might be down!")
 		return
 	}
 	fmt.Println(link, "is up!")
